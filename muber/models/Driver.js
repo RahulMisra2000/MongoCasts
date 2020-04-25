9873085e6b55fd90320133033ed4ca552c61936e
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2020-04-25 12:43:21 -0400

    Using GeoJson for storing Driver's location data

--- muber/models/Driver.js
+++ muber/models/Driver.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PointSchema = new Schema({
-  coordinates: { type: [Number], index: '2dsphere' },
-  type: { type: String, default: 'Point' }
+  coordinates: { 
+      type: [Number],                 // This is for lat and lng 
+      index: '2dsphere'               // want mongo to use 2dsphere way instead of 2d because of .... earth distances
+  },
+  type: { 
+      type: String, default: 'Point'  // GeoJson supports, Point, Line, Polygon, etc
+                                      // we have chosen point because we are concerned with a driver's location
+  }
 });
 
 const DriverSchema = new Schema({
